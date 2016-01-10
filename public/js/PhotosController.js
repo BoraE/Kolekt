248ea12e05f3d2098a92c735da4292b889cdd0be
Bora Eryilmaz <bora.eryilmaz@gmail.com>
2016-01-09 21:43:53 -0500

    Introduced Photos controller class

---
+++ public/js/PhotosController.js
@@ -0,0 +1,75 @@
+define(['Button'], function(Button) {
+    'use strict';
+
+    function PhotosController(el) {
+        this.domNode = document.querySelector(el);
+        this.title = this.domNode.querySelector('.section_title');
+        this.content = this.domNode.querySelector('.section_content');
+        this.addButton = new Button('#add_photo');
+
+        this.initialize();
+    }
+
+    PhotosController.prototype.initialize = function() {
+        var input = this.content.querySelector('input[type=file]');
+        input.addEventListener('change', handleFiles.bind(this), false);
+
+        this.addButton.addEventListener('click', function(e) {input.click();});
+        this.title.addEventListener("click", handlePhotoSection.bind(this), false);
+    };
+
+    PhotosController.prototype.addImageFromFile = function(url, file) {
+        var el = document.createElement('img');
+        el.src = url;
+        el.File = file;
+        el.addEventListener('click', function(e) {
+            var selected;
+            e.target.classList.toggle('selected');
+            // selected = this.content.querySelectorAll('img.selected');
+            // deleteButton.setEnabled(selected.length > 0);
+        });
+        this.content.insertBefore(el, this.addButton.domNode);
+    };
+
+    PhotosController.prototype.removeSelectedImages = function(url, file) {
+        var selected = this.content.querySelectorAll('img.selected');
+        Array.prototype.forEach.call(selected, function(el) {
+            this.content.removeChild(el);
+        });
+        // deleteButton.setEnabled(false);
+    };
+
+    PhotosController.prototype.getImageFiles = function() {
+        var files = [];
+        var images = this.content.querySelectorAll('img');
+        Array.prototype.forEach.call(images, function(el) {
+            files.push(el.File);
+        });
+        return files;
+    };
+
+    function handlePhotoSection(e) {
+        this.title.classList.toggle('collapsed');
+        this.content.classList.toggle('hide');
+    }
+
+    function handleFiles(e) {
+        var file = e.target.files[0];
+        if (file.type.search('image/*') == -1) {
+            alert("Selected file is not an image.");
+            return;
+        }
+
+        var reader = new FileReader();
+        var self = this;
+        reader.onload = function(e) {
+            self.addImageFromFile(e.target.result, file);
+        };
+        reader.onerror = function() {
+            alert("Could not load selected image.");
+        };
+        reader.readAsDataURL(file);
+    }
+
+    return PhotosController;
+});
--- public/js/app.js
+++ public/js/app.js
@@ -1,8 +1,9 @@
-define(['form-navigator', 'Button'], function(FormNavigator, Button) {
+define(['form-navigator', 'Button', 'PhotosController'], function(FormNavigator, Button, PhotosController) {
 
     var sendButton;
     var loginButton;
     var deleteButton;
+    var photosController;
 
     function initialize() {
         console.log('Loading app.js...');
@@ -15,64 +16,32 @@ define(['form-navigator', 'Button'], function(FormNavigator, Button) {
     }
 
     function setup() {
-        var photo_title = document.querySelector('#section_photos > .section_title');
-        //var map_title = document.querySelector('#map > .section_title');
+        photosController = new PhotosController('#section_photos');
+
+        var map_title = document.querySelector('#section_map > .section_title');
         var form_title = document.querySelector('#section_form > .section_title');
 
-        photo_title.addEventListener("click", handlePhotoSection);
-
-        // map_title.addEventListener("click", function(e) {
-        //     var map_content = document.querySelector('#map > .section_content');
-        //     map_content.classList.toggle('hide');
-
-        // });
+        map_title.addEventListener("click", function(e) {
+            var map_content = document.querySelector('#section_map > .section_content');
+            map_content.classList.toggle('hide');
+            map_title.classList.toggle('collapsed');
+        });
 
         form_title.addEventListener("click", function(e) {
             var form_content = document.querySelector('#section_form > .section_content');
             form_content.classList.toggle('hide');
-
+            form_title.classList.toggle('collapsed');
         });
-    }
-
-    function handlePhotoSection(e) {
-        var photo_content = document.querySelector('#section_photos > .section_content');
-        photo_content.classList.toggle('hide');
     }
 
     function setup2() {
         var fn = new FormNavigator();
         fn.placeAt('#navigation');
 
-        var camera = document.querySelector('#camera');
-        var map = document.querySelector('#map');
-        var add = document.querySelector('#add');
-
-        var input = document.querySelector('input[type=file]');
-
-        add.addEventListener('click', function(e) {input.click();}, false);
-        input.addEventListener('change', handleFiles, false);
-
         sendButton.addEventListener('click', sendFiles);
-        deleteButton.addEventListener('click', removeSelectedImages);
+        deleteButton.addEventListener('click', photosController.removeSelectedImages);
         loginButton.setEnabled(false);
         deleteButton.setEnabled(false);
-
-        map.addEventListener('click', function(e) {
-            var location = document.getElementById('location');
-            var images = document.querySelector('#section_photos > .section_content');
-            location.classList.remove('hide');
-            camera.classList.remove('hide');
-            map.classList.add('hide');
-            images.classList.add('hide');
-        });
-        camera.addEventListener('click', function(e) {
-            var location = document.getElementById('location');
-            var images = document.querySelector('#section_photos > .section_content');
-            location.classList.add('hide');
-            camera.classList.add('hide');
-            map.classList.remove('hide');
-            images.classList.remove('hide');
-        });
     }
 
     function sendFiles(e) {
@@ -94,66 +63,17 @@ define(['form-navigator', 'Button'], function(FormNavigator, Button) {
         }
     }
 
-    function handleFiles(e) {
-        var file = e.target.files[0];
-        if (file.type.search('image/*') == -1) {
-            alert("Selected file is not an image.");
-            return;
-        }
-        var images = document.querySelector('#section_photos > .section_content');
-
-        var reader = new FileReader();
-        reader.onload = function(e) {
-            addImageFromFile(e.target.result, file);
-        };
-        reader.onerror = function() {
-            alert("Image did not load.");
-        };
-        reader.readAsDataURL(file);
-    }
-
     // Construct form data to be sent over to the server
     function getFormData() {
         var form = document.querySelector('form');
         var formData = new FormData(form);
-        var images = document.querySelectorAll('#section_photos .section_content > img');
+        var files = photosController.getImageFiles();
 
-        Array.prototype.forEach.call(images, function(el) {
-            var file = el.File;
+        Array.prototype.forEach.call(files, function(file) {
             console.log(file);
             formData.append(file.name, file);
         });
         return formData;
-    }
-
-    // Add add to the top banner
-    function addImageFromFile(url, file) {
-        var container = document.querySelector('#section_photos > .section_content');
-        var el = document.createElement('img');
-        el.src = url;
-        el.File = file;
-        el.addEventListener('click', function(e) {
-            var selected;
-            e.target.classList.toggle('selected');
-            selected = container.querySelectorAll('img.selected');
-            deleteButton.setEnabled(selected.length > 0);
-        });
-        container.appendChild(el);
-        container.classList.remove('disabled');
-    }
-
-    // Remove selected images from the top banner
-    function removeSelectedImages() {
-        var container = document.querySelector('#section_photos > .section_content');
-        var images = container.querySelectorAll('img.selected');
-        Array.prototype.forEach.call(images, function(el) {
-            container.removeChild(el);
-        });
-        // If all images are removed bring back the information banner
-        if (container.querySelectorAll('img').length === 0) {
-            container.classList.add('disabled');
-        }
-        deleteButton.setEnabled(false);
     }
 
     return {initialize: initialize};
